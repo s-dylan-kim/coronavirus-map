f9bc7bf62fffdc4a1c37e1e0e4512f7a4511ec5e
Dylan Kim <s.dylan.kim@gmail.com>
2020-05-26 02:13:00 -0500

    added leaflet to website

---
+++ angular-src/src/app/map-view/map-view.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map-view',
+  templateUrl: './map-view.component.html',
+  styleUrls: ['./map-view.component.css']
+})
+export class MapViewComponent implements OnInit, AfterViewInit {
+  private map;
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+  private initMap(): void {
+    this.map = L.map('mapid', {
+      center: [ 39.8282, -98.5795 ],
+      zoom: 3
+    });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+  }
+
+}
