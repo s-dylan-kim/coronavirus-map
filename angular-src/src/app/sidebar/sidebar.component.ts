8214ceac97bf83e3f15c4e7598d8fcd8eb27f47e
Dylan Kim <s.dylan.kim@gmail.com>
2020-05-27 02:11:21 -0500

    added file upload service and also added an upload bar

--- angular-src/src/app/sidebar/sidebar.component.ts
+++ angular-src/src/app/sidebar/sidebar.component.ts
@@ -1,20 +1,60 @@
-import { Component, OnInit } from '@angular/core';
-
-
-
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { HistoryUploadService } from "../location-history-upload/history-upload.service"
+import { HttpEvent, HttpEventType } from '@angular/common/http';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent{
+  form: FormGroup;
+  progress = 0;
 
-  constructor() {}
+  constructor(
+    public fb: FormBuilder,
+    public historyUploadService: HistoryUploadService
+  ) {
+    this.form = this.fb.group({
+      history: [null]
+    })
+  }
 
   ngOnInit(): void {
   }
 
+  onFileUpload(event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({
+      history: file
+    });
+    this.form.get('history').updateValueAndValidity()
+  }
 
+  submitFile() {
+    console.log("boo!");
+  //   this.historyUploadService.uploadFile(
+  //     this.form.value.history
+  //   ).subscribe((event: HttpEvent<any>) => {
+  //     switch (event.type) {
+  //       case HttpEventType.Sent:
+  //         console.log('Request has been made!');
+  //         break;
+  //       case HttpEventType.ResponseHeader:
+  //         console.log('Response header has been received!');
+  //         break;
+  //       case HttpEventType.UploadProgress:
+  //         this.progress = Math.round(event.loaded / event.total * 100);
+  //         console.log(`Uploaded! ${this.progress}%`);
+  //         break;
+  //       case HttpEventType.Response:
+  //         console.log('User successfully created!', event.body);
+  //         setTimeout(() => {
+  //           this.progress = 0;
+  //         }, 1500);
 
+  //     }
+  //   })
+  }
 }
